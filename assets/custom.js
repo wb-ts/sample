e6f8aa8861ba39b66131015313de7dc67f66dafb
SenDev001 <sergeilipovsky@gmail.com>
2021-11-03 10:16:46 +0800

    theme

--- assets/custom.js
+++ assets/custom.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-  var isAnnouncement = localStorage.getItem('isAnnouncement');
-  if(isAnnouncement != "yes") {
-    $(".sqs-announcement-bar-text").removeClass("hide");
-  }
-  $(".side_bar_button").click(function () {
-    $(this).toggleClass("is_active");
-    $(this).next().slideToggle();
-  });
-  $("#BtnMobileMenu").click(function() {
-    $(".mobile-nav-wrapper").addClass("is--active");
-  });
-  $(".mobile-menu-close").click(function() {
-    $(".mobile-nav-wrapper").removeClass("is--active");
-  });
-  // Zoom plugin
-  // $('.ik-product-zoom-image').imageZoom();
-  $('.ik-image-zoom').each(function(){
-		$(this).imageZoom();
-	});
-  // Announcement Bar
-  $(".sqs-announcement-bar-close").click(function() {
-    localStorage.setItem('isAnnouncement', "yes");
-    $(".sqs-announcement-bar-text").addClass("hide");
-  });
+$(document).ready(function () {
+  var isAnnouncement = localStorage.getItem('isAnnouncement');
+  if(isAnnouncement != "yes") {
+    $(".sqs-announcement-bar-text").removeClass("hide");
+  }
+  $(".side_bar_button").click(function () {
+    $(this).toggleClass("is_active");
+    $(this).next().slideToggle();
+  });
+  $("#BtnMobileMenu").click(function() {
+    $(".mobile-nav-wrapper").addClass("is--active");
+  });
+  $(".mobile-menu-close").click(function() {
+    $(".mobile-nav-wrapper").removeClass("is--active");
+  });
+  // Zoom plugin
+  // $('.ik-product-zoom-image').imageZoom();
+  $('.ik-image-zoom').each(function(){
+		$(this).imageZoom();
+	});
+  // Announcement Bar
+  $(".sqs-announcement-bar-close").click(function() {
+    localStorage.setItem('isAnnouncement', "yes");
+    $(".sqs-announcement-bar-text").addClass("hide");
+  });
 })
